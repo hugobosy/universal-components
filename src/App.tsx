4f3a3cc3449c6c735a8f73a71012ffb574981bad
HugoBosy <hubert.kliszcz@gmail.com>
2022-12-14 04:14:52 +0100

    add parallax component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ButtonComponent } from "./components/button/Button.components";
+import { ParallaxComponent } from "./components/parallax/Parallax.component";
 
 function App() {
   return (
     <div className="App">
       <ButtonComponent text="Tekst przycisku" />
+      <ParallaxComponent />
     </div>
   );
 }
--- src/components/parallax/Parallax.component.tsx
+++ src/components/parallax/Parallax.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { Wrapper } from "./Parallax.styles";
 
 export const ParallaxComponent = () => {
-  return <div></div>;
+  return <Wrapper></Wrapper>;
 };
